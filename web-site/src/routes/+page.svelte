0d498a6d5e3905b34d6732866769ec99ed3000ad
ohmdob <ohm10513@gmail.com>
2022-10-27 01:06:15 +0700

    init

---
+++ web-site/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div>
+	Hello World Static
+</div>
---
+++ web-site/svelte.config.js
@@ -0,0 +1,11 @@
+// svelte.config.js
+import adapter from '@sveltejs/adapter-static';
+
+export default {
+  kit: {
+    adapter: adapter({
+      fallback: 'index.html'
+    }),
+    prerender: { entries: [] }
+  }
+};
